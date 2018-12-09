1fd2b6236debef50d8afb167b0199519d182f7bc
Raj <Raj>
2018-12-09 22:49:23 +0530

    ready to push

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,23 @@
-console.log("hi");
+import React from "react";
+import {render} from "react-dom";
+
+import {Header} from "./components/Header";
+import {Home} from "./components/Home";
+
+class App extends React.Component{
+	render(){
+		return(
+			<div className="components">
+				<div>
+				<Header/>
+				</div>
+				<div>
+				<Home/>
+				</div>
+			</div>
+			);
+	}
+}
+
+
+render(<App/>, window.document.getElementById("app"));
